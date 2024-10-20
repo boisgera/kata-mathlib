e48c0ba9e89eb2a8aa3e78bdac9e9f8fee7743f0
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2024-10-20 23:46:42 +0200

    wip ex 2

--- ex2.lean
+++ ex2.lean
@@ -8,29 +8,32 @@ Let $n$ and $k$ be nonnegative integers with $k \leqslant n$. Then
 (ii) $\binom{n}{k}=\binom{n}{n-k}$.
 -/
 
-section
+lemma ex_lb_succ (n : ℕ) : ∀ (k : ℕ), n < k → ∃ j, k = succ j := by
+  intro k
+  cases k with
+  | zero => intro ; contradiction
+  | succ m => intro ; use m
 
-variables (k n : ℕ)
-
-lemma lemma₀ : (n < k) -> choose n k = 0 := by
+lemma lemma₀ (n : ℕ) : ∀ (k : ℕ), (n < k) -> choose n k = 0 := by
   induction n with
   | zero =>
-    intro zero_lt_k
-    have ⟨j, k_is_succ_j⟩ : ∃ j : ℕ, k = j + 1 := by
-      cases k with
-      | zero =>
-        have h := Nat.lt_irrefl 0
-        contradiction
-      | succ j =>
-        use j
-    rw [k_is_succ_j]
-    rw [choose]
-  | succ j ih =>
-    -- 🤯 Brainz goes brrrrrrrrrr!
-    -- need to tame the pattern matching used in the definition of choose
-    sorry
+    intro k n_lt_k
+    let ⟨_, h⟩ := ex_lb_succ 0 k n_lt_k
+    rw [h, choose]
+  | succ n ih =>
+    intro k succ_n_lt_k
+    have ⟨m, hm⟩ := ex_lb_succ (n+1) k succ_n_lt_k
+    rw [hm, choose]
+    have le_1 : n < m := by
+      subst hm
+      simp only [succ_eq_add_one, add_lt_add_iff_right] at succ_n_lt_k
+      assumption
+    have le_2: n < m + 1 := by
+      subst hm
+      exact lt_trans le_1 (lt_add_one m)
+    rw [ih m le_1, ih (m+1) le_2]
 
-lemma lemma₁ : choose n 0 = 1 ∧ choose n n = 1 := by
+theorem theorem₁ (n : ℕ) : choose n 0 = 1 ∧ choose n n = 1 := by
   constructor
   . rw[choose]
   . induction n with
@@ -38,17 +41,14 @@ lemma lemma₁ : choose n 0 = 1 ∧ choose n n = 1 := by
       rw[choose]
     | succ n ih =>
       rw [choose, ih]
-      have : n < n + 1 := by
-        sorry
-      rw [lemma₀ (n+1) n this]
+      have : n < n + 1 := lt_add_one n
+      rw [lemma₀ n (n+1) this]
 
-lemma lemma₂ :  (k ≤ n) -> choose n k = choose n (n - k) := by
-  intro k_le_n
+theorem theorem₂ (n : ℕ) : ∀ (k : ℕ), (k ≤ n) -> choose n k = choose n (n - k) := by
+  intro k k_le_n
   induction n with
   | zero =>
     simp only [nonpos_iff_eq_zero] at k_le_n
     simp only [_root_.zero_le, Nat.sub_eq_zero_of_le, choose_self, k_le_n]
   | succ n ih =>
     sorry
-
-end
