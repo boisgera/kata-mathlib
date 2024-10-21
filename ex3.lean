3bc0388323ec6f6011143fd6348d9333b13e1cd4
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2024-10-21 15:52:40 +0200

    Add skeleton for ex 3

--- ex3.lean
+++ ex3.lean
@@ -1,6 +1,24 @@
+import Mathlib
+
+open Nat
+
 /-
-We define a function recursively for all positive integers $n$ 
-by $f(1)=1$, $f(2)=5$, and for $n>2, f(n+1)=f(n)+2 f(n-1)$. 
-Show that $f(n)=$ $2^{n}+(-1)^{n}$, 
+We define a function recursively for all positive integers $n$
+by $f(1)=1$, $f(2)=5$, and for $n>2, f(n+1)=f(n)+2 f(n-1)$.
+Show that $f(n)= 2^{n}+(-1)^{n}$,
 using the second principle of mathematical induction.
 -/
+
+def f : ℕ -> ℤ
+| 0 => 2 -- 2^0 + (-1)^0
+| 1 => 1
+| 2 => 5
+| n + 2 => (f n) + 2 * (f (n+1))
+
+#check Nat.strong_induction_on
+-- theorem Nat.strong_induction_on
+-- {p : ℕ → Prop} (n : ℕ) (h : ∀ (n : ℕ), (∀ (m : ℕ), m < n → p m) → p n) :
+-- p n
+
+theorem f_closed_form (n : ℕ) : f n = 2^n + (-1)^n := by
+  sorry
