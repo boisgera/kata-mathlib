44462f59324565f4775a5cd640283a9be1b77589
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2024-10-21 23:21:11 +0200

    Clean-up

--- ex3.lean
+++ ex3.lean
@@ -27,18 +27,17 @@ theorem f_closed_form : ∀ n : ℕ, f n = 2^n + (-1)^n := by
   | 2 => intro ; rw [f]; rfl
   | n + 2 =>
     intro p
-    have p_n : f n = 2^n + (-1)^n := by
+    have p_n : f n = 2 ^ n + (-1) ^ n := by
       have : n < n + 2 := by
         simp [Int.reduceNeg, zero_lt_one, ofNat_pos, lt_add_iff_pos_right]
       exact p n this
-    have p_succ_n : f (n + 1) = 2^(n + 1) + (-1)^(n + 1) := by
+    have p_succ_n : f (n + 1) = 2 ^ (n + 1) + (-1) ^ (n + 1) := by
       have : n + 1 < n + 2 := by
         simp [Int.reduceNeg, zero_lt_one, ofNat_pos, lt_add_iff_pos_right]
       exact p (n+1) this
     by_cases h : n > 0
     . calc f (n + 2)
-      _ = f (n + 1) + 2 * f n := by
-        rw [f] ; intro n_eq_zero ; rw [n_eq_zero] at h ; contradiction
+      _ = f (n + 1) + 2 * f n := by rw [f] ; intro n_eq_zero ; rw [n_eq_zero] at h ; contradiction
       _ = 2 ^ (n + 1) + (-1) ^ (n + 1) + 2 * (2 ^ n + (-1) ^ n) := by rw [p_n, p_succ_n]
       _ = 2 ^ (n + 2) + (-1) ^ (n + 2) := by ring
     . simp only [gt_iff_lt, not_lt, nonpos_iff_eq_zero] at h
