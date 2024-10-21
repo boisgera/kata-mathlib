5d5227ba4dc3cb92ba9a79d04a0ecd0a222bd61d
Sébastien Boisgérault <Sebastien.Boisgerault@gmail.com>
2024-10-21 18:23:48 +0200

    Add detailled skeleton for ex 3

--- ex3.lean
+++ ex3.lean
@@ -10,15 +10,41 @@ using the second principle of mathematical induction.
 -/
 
 def f : ℕ -> ℤ
-| 0 => 2 -- 2^0 + (-1)^0
+| 0 => 2 -- 2 ^ 0 + (-1) ^ 0, so that the closed-form also works for n = 0.
 | 1 => 1
 | 2 => 5
-| n + 2 => (f n) + 2 * (f (n+1))
+| n + 2 => f (n + 1) + 2 * f n
 
 #check Nat.strong_induction_on
--- theorem Nat.strong_induction_on
--- {p : ℕ → Prop} (n : ℕ) (h : ∀ (n : ℕ), (∀ (m : ℕ), m < n → p m) → p n) :
--- p n
 
-theorem f_closed_form (n : ℕ) : f n = 2^n + (-1)^n := by
-  sorry
+theorem f_closed_form : ∀ n : ℕ, f n = 2^n + (-1)^n := by
+  intro
+  apply Nat.strong_induction_on (p := fun (n : ℕ) => f n = 2^n + (-1)^n)
+  intro n
+  match n with
+  | 0 => intro ; rw [f]; rfl
+  | 1 => intro ; rw [f]; rfl
+  | 2 => intro ; rw [f]; rfl
+  | n + 2 =>
+    intro p
+    have p_n : f n = 2^n + (-1)^n := by
+      have : n < n + 2 := by
+        simp [Int.reduceNeg, zero_lt_one, ofNat_pos, lt_add_iff_pos_right]
+      exact p n this
+    have p_succ_n : f (n + 1) = 2^(n + 1) + (-1)^(n + 1) := by
+      have : n + 1 < n + 2 := by
+        simp [Int.reduceNeg, zero_lt_one, ofNat_pos, lt_add_iff_pos_right]
+      exact p (n+1) this
+    by_cases h : n > 0
+    . calc f (n + 2)
+      _ = f (n + 1) + 2 * f n := by rw [f] ; intro n_eq_zero ; rw [n_eq_zero] at h ; contradiction
+      _ = 2 ^ (n + 1) + (-1) ^ (n + 1) + 2 * (2 ^ n + (-1) ^ n) := by rw [p_n, p_succ_n]
+      _ = 2 ^ (n + 1) + (-1) ^ (n + 1) + 2 * 2 ^ n + 2 * (-1) ^ n := by sorry
+      _ = 2 ^ (n + 1) + (-1) ^ (n + 1) + 2 ^ (n + 1) + 2 * (-1) ^ n := by sorry
+      _ = 2 ^ (n + 1) + 2 ^ (n + 1) + ((-1) ^ (n + 1) + 2 * (-1)^n) := by sorry
+      _ = 2 ^ (n + 2) + (-1) ^ (n + 2) := by sorry
+    . simp only [gt_iff_lt, not_lt, nonpos_iff_eq_zero] at h
+      subst h
+      simp only [zero_add, Int.reducePow, Int.reduceNeg, even_two, Even.neg_pow, one_pow,
+        Int.reduceAdd]
+      rw [f]
